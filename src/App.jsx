1e2d001d2eb78cc69d9300cfcee2733d48ecb164
Manuel <begambresuarezm@gmail.com>
2026-03-20 16:13:32 -0500

    new: add new pages in a good way

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Creators from "./pages/Creators";
+import Characters from "./pages/Characters";
 import './css/App.css'
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="layout">
       <Header />
-      <Home />
+
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/creators" element={<Creators />} />
+          <Route path="/characters" element={<Characters />} />
+        </Routes>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
